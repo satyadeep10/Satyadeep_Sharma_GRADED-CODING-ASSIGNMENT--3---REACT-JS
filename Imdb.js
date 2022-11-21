4bbdadfe38d7a02fd160adef07f7a976426f573d
satyadeep10 <99476026+satyadeep10@users.noreply.github.com>
2022-11-21 21:23:14 +0530

    "Unable to create separate folders hence imp files

---
+++ Imdb.js
@@ -0,0 +1,19 @@
+import React from 'react';
+  
+const Imdb = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Left',
+        alignItems: 'Left',
+        color:'white',
+        height: '100vh'
+      }}
+    >
+      <h1>Imdb</h1>
+    </div>
+  );
+};
+  
+export default Imdb;
