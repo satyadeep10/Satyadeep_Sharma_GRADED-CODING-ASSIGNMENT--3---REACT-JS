eb45d6ab6689a66fe175da3dcdad84ab0ffd7d9c
satyadeep10 <99476026+satyadeep10@users.noreply.github.com>
2023-01-15 12:46:37 +0530

    uploaded

---
+++ react-movie-app copy/src/App.js
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Imdb from "./Imdb";
+
+export const API_KEY = "89e831c7";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+  aling-items: center;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState();
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const fetchData =async(searchString) => {
+    const response = await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`);
+    console.log(response)
+    updateMovieList(response.data.Search)
+
+  };
+  
+  const onTextChange = (event) => {
+       clearTimeout(timeoutId);
+       updateSearchQuery(event.target.value);
+       const timeout = setTimeout(() => fetchData (event.target.value), 500);
+       updateTimeoutId(timeout);
+      };
+  
+   return(
+  <Container>
+   <Header>
+      <AppName>
+        <MovieImage src="/movie-icon.svg"/>
+        React Movie App
+      </AppName>
+      <SearchBox>
+        <SearchIcon src="/search-icon.svg"/>
+          <SearchInput placeholder = "Search Movie"
+          value={searchQuery} 
+          onChange={onTextChange}/>  
+      </SearchBox>
+    </Header> 
+    
+    {selectedMovie && (
+    <MovieInfoComponent 
+    selectedMovie={selectedMovie}
+    onMovieSelect={onMovieSelect}
+    />
+   )}
+    <MovieListContainer>
+    {movieList?.length
+    ? movieList.map((movie,index) => (
+    <MovieComponent key ={index} movie= 
+    {movie} onMovieSelect={onMovieSelect}/>
+    )): 
+  
+    <Placeholder src="/movie-icon.svg"/>
+           
+    }
+
+    </MovieListContainer>
+    
+  </Container>
+    );
+
+
+    return (
+      <Router>
+        
+        <Routes>
+          
+          <Route path='/Imdb' component={Imdb} />
+         
+        </Routes>
+      </Router>
+    );
+
+}
+
+export default App;
+
+
+
+
+
